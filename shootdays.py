35c55bdcb461c21010fdd98308521e2d687cc4f5
magenta-em <152385472+magenta-em@users.noreply.github.com>
2023-12-07 15:47:21 +0000

    Update shootdays.py

--- shootdays.py
+++ shootdays.py
@@ -2,7 +2,7 @@ import os
 import arrow
 
 def main():
-	findShootday("/Volumes/ARECA_88TB/SONIC3")
+	findShootday("/Users/download/Desktop/ARECA_88TB/SONIC3/")
 
 def findShootday(base):
 	today = arrow.now().format('YYYYMMDD')
@@ -12,29 +12,27 @@ def findShootday(base):
 	
 	while today_sd == "":
  		for dir in os.listdir(base):
-			if dir[:8] == today:
-				today_dirs.append(dir)
-		if len(today_dirs) > 1:
-			print("Multiple Shootdays made today")
-			while today_sd = "":
-				#work out how to find the most up to date shootday for today
-		if len(today_dirs) == 1:
-			today_sd = today_dirs[0]
-		if len(today_dirs) < 1:
-			#Assume Nightwork
-			print("No shootdays made today, assuming nightwork")
-			yesterday = arrow.now().replace(days=-1).format('YYYYMMDD')
-			for dir in os.listdir(base):
-				if dir[:8] == yesterday:
-					yday_dirs.append(dir)
-			if len(yday_dirs) > 1:
-				print("Multiple shootdays yesterday, unclear selection")
-				#Work out how to select from list
-			if len(yday_dirs) < 1:
-				print("No shootdays today or yesterday, unclear process.\nExiting")
-				exit()
-			if len(yday_dirs) == 1:
-				today_sd = yday_dirs[0]
+ 			if dir[:8] == today:
+ 				today_dirs.append(dir)
+ 				print(dir)
+ 		if len(today_dirs) > 1:
+ 			print("Multiple Shootdays made today")
+ 			#while today_sd == "":
+ 			#work out how to find the most up to date shootday for today
+ 		if len(today_dirs) == 1:
+ 			today_sd = today_dirs[0]
+ 		if len(today_dirs) < 1:
+ 			print("No shootdays made today, assuming nightwork")
+ 			yesterday = arrow.now().shift(days=-1).format('YYYYMMDD')
+ 			if dir[:8] == yesterday:
+ 				yday_dirs.append(dir)
+ 			if len(yday_dirs) > 1:
+ 				print("Multiple shootdays yesterday, unclear selection")
+ 			if len(yday_dirs) < 1:
+ 				print("No shootdays today or yesterday, unclear process.\nExiting")
+ 				exit()
+ 			if len(yday_dirs) == 1:
+ 				today_sd = yday_dirs[0]
 				
 	print("Your shootday is: " + today_sd)
 	return today_sd
@@ -42,7 +40,3 @@ def findShootday(base):
 
 if __name__ == "__main__":
 	main()
-				
-		
-			
-		
